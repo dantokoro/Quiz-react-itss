2f21b3b9a65f18318eaf82dc6025266bb43d157b
Bach Do <dohuybach714@gmail.com>
2020-04-02 23:26:36 +0700

    Add Basic Categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from "react";
 import "./App.css";
 import data from "./components/data.json";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Button, Row, Col, Container } from "react-bootstrap";
+import CategoryBar from "./CategoryBar.js";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      questionList: data
+      questionList: data,
+      category: "All"
     };
   }
+  handleClickAll() {
+    this.setState({
+      category: "All"
+    });
+  }
+  handleClickCategory(category) {
+    this.setState({
+      category: category
+    });
+  }
   render() {
+    const categorySet = new Set();
+    this.state.questionList.map(item => {
+      categorySet.add(item.category);
+    });
+    const categoryList = Array.from(categorySet);
+    const questions = this.state.questionList.map(item => {
+      if (this.state.category === "All") {
+        return <p>{item.question}</p>;
+      } else {
+        if (this.state.category === item.category) {
+          return <p>{item.question}</p>;
+        }
+      }
+    });
     return (
-    <div className="App">
-      {this.state.questionList.map((item, index) => (
-        <p>{item.question}</p>
-      ))}
-    </div>
+      <div className="App">
+        <Container>
+          <Row className="mt-3 justify-content-md-center">
+            <Col>
+              <Button
+                className="px-5"
+                variant="outline-primary"
+                onClick={() => this.handleClickAll()}
+              >
+                All
+              </Button>
+            </Col>
+          </Row>
+          <Row className="mt-3 justify-content-md-center">
+            <CategoryBar
+              titles={categoryList}
+              onClick={category => this.handleClickCategory(category)}
+            ></CategoryBar>
+          </Row>
+        </Container>
+        {questions}
+      </div>
     );
   }
 }
-
+// {this.state.questionList.map((item, index) => (
+//   <p>{item.question}</p>
+// ))}
 export default App;
