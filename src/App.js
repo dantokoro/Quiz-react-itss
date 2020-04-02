e99bd88dac0b0c53cf7a212680b8a03472dd8540
Dantokoro <duongdang0508@gmail.com>
2020-04-02 09:40:20 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import data from "./components/data.json";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <p>
-          {data.map( item => (
-            <p>{item.question}</p>
-          ))}
-        </p>
+        {data.map(item => (
+          <p>{item.question}</p>
+        ))}
+      </p>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src="https://i.pinimg.com/originals/b4/f9/a7/b4f9a703222d96c5ed72ac1b94aeca4e.png"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
