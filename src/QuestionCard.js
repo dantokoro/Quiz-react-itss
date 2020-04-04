291cc1fb229fc538082c5231f3045ed5f347f514
Duong Le <tduongle10@gmail.com>
2020-04-04 17:07:01 +0700

    show answer after select

--- src/QuestionCard.js
+++ src/QuestionCard.js
@@ -4,39 +4,53 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Col, Form } from "react-bootstrap";
 
 class QuestionCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: null
+        }
+    }
+    handleSelect = (option) => {
+        this.setState({ selected: option })
+    }
     render() {
         return (
             <div>
                 <Card className="Question-card">
                     <Card.Header>{this.props.item.question}</Card.Header>
                     <Card.Body>
-                        <Form>
+                        <Form className="Question-form">
                             <Form.Check
-                                name="question"
+                                name="option"
                                 type="radio"
                                 id="default-radio"
                                 label={`a: ${this.props.item.a}`}
+                                onChange={() => this.handleSelect('a')}
                             />
                             <Form.Check
-                                name="question"
+                                name="option"
                                 type="radio"
                                 id="default-radio"
                                 label={`b: ${this.props.item.b}`}
+                                onChange={() => this.handleSelect('b')}
                             />
                             <Form.Check
-                                name="question"
+                                name="option"
                                 type="radio"
                                 id="default-radio"
                                 label={`c: ${this.props.item.c}`}
+                                onChange={() => this.handleSelect('c')}
                             />
                             <Form.Check
-                                name="question"
+                                name="option"
                                 type="radio"
                                 id="default-radio"
                                 label={`d: ${this.props.item.d}`}
+                                onChange={() => this.handleSelect('d')}
                             />
                         </Form>
-                        <div>Answer: {this.props.item.answer}</div>
+                        { this.state.selected !== null ? <div>Answer: {this.props.item.answer}</div> : null }
+                        
                     </Card.Body>
                 </Card>
             </div>
